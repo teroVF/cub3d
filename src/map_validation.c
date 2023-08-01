add2809b48ce5b97a6a1e2733b454bfb52c7b7bd
teroVF <anvieira@student.42porto.com>
2023-08-01 03:01:10 +0100

    maps and arguments errors

--- src/map_validation.c
+++ src/map_validation.c
@@ -1,24 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   map_validation.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anvieira <anvieira@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/31 01:33:02 by anvieira          #+#    #+#             */
-/*   Updated: 2023/08/01 02:59:53 by anvieira         ###   ########.fr       */
+/*   Created: 2023/08/01 01:49:56 by anvieira          #+#    #+#             */
+/*   Updated: 2023/08/01 01:54:02 by anvieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/cub3d.h"
-
-int main(int ac, char** av)
+static int	valid_cub(char *map_file)
 {
-	if (ac != 2)
-		error_msg(INVALID_NBR_ARGS);
-	if (argv[1] == NULL)
-		error_msg(NULL_MAP);
-	if (!valid_cub(argv[1]))
-		error_msg(INVALID_MAP_FILE);
-	return (EXIT_SUCCESS);
+	size_t	i;
+
+	i = ft_strlen(map_file) - 4;
+	if (ft_strncmp(".cub", &map_file[i], 4) == 0)
+		return (1);
+	return (0);
+}
+
+void	error_msg(char *error_msg)
+{
+	ft_putstr_fd("Error\n", STDERR_FILENO);
+	ft_putendl_fd(error_msg, STDERR_FILENO);
+    return (EXIT_FAILURE);
 }
