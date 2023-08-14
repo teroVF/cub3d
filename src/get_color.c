861fcb665ecce46cb494cb80dedc679b9728a5dc
wcorrea- <waltergccruz@gmail.com>
2023-08-14 15:45:03 +0100

    fix N & W start texture point

--- src/get_color.c
+++ src/get_color.c
@@ -6,7 +6,7 @@
 /*   By: wcorrea- <wcorrea-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 00:47:57 by anvieira          #+#    #+#             */
-/*   Updated: 2023/08/14 14:29:01 by wcorrea-         ###   ########.fr       */
+/*   Updated: 2023/08/14 15:41:55 by wcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@ int	obtain_color(t_game *cub3d)
 	if (cub3d->player.hit_side)
 	{
 		if (cub3d->player.pos.y > cub3d->player.map_square.y)
-			//textura e coordenadas
 			return (obtain_color_pixel(&cub3d->north_img, x, y));
 		else
 			return (obtain_color_pixel(&cub3d->south_img, x, y));
@@ -47,13 +46,21 @@ void	find_out_text_x(t_player *player)
 {
 	double	wall;
 
-	//se bateu na parede na horizontal ou vertical
 	if (!player->hit_side)
-		wall = player->pos.y + player->size_ray * player->ray_dir.y;
+	{
+		if (player->pos.x < player->map_square.x)
+			wall = player->pos.y + player->size_ray * player->ray_dir.y;
+		else
+			wall = player->pos.y - player->size_ray * player->ray_dir.y;
+	}
 	else
-		wall = player->pos.x + player->size_ray * player->ray_dir.x;
+	{
+		if (player->pos.y > player->map_square.y)
+			wall = player->pos.x - player->size_ray * player->ray_dir.x;
+		else
+			wall = player->pos.x + player->size_ray * player->ray_dir.x;
+	}
 	wall -= floor(wall);
-	//onde começa a textura
 	player->text_x = (int)(wall * (double)TEXTURE_SIZE);
 	printf("text_x: %d\n", player->text_x);
 }
