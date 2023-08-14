92d5be66184e336491167b1860bfab1497e56030
anvieira <anvieira@student.42porto.com>
2023-08-14 04:25:06 +0100

    add text_y

--- includes/cub.h
+++ includes/cub.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anvieira <anvieira@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: anvieira <anvieira@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 00:35:31 by anvieira          #+#    #+#             */
-/*   Updated: 2023/08/13 03:41:10 by anvieira         ###   ########.fr       */
+/*   Updated: 2023/08/14 04:24:27 by anvieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,6 +120,7 @@ typedef struct s_game
 	int			empty_line;
 	t_data		frame;
 	t_player	player;
+	int			step_y;
 }				t_game;
 
 /* PARSE.C */
@@ -187,8 +188,8 @@ void		free_split(char **split);
 void		clean_parse(t_game *cub3d);
 int			end_game(t_game *game);
 
-int			obtain_color(t_game *cub3d, int y);
-void		discover_textere_x (t_player player);
+int			obtain_color(t_game *cub3d);
+void		discover_textere_x (t_player *player);
 void		init_textures(t_game *cub3d);
 
 #endif
